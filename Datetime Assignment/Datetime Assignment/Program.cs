3e989cbb78fb91ebd812d5e122c8bec6de54f43d
micuch11 <micuch1126@gmail.com>
2024-03-26 05:58:53 +0000

    Datetime Assignment

---
+++ Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Datetime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Print the current date and time to the console
+            Console.WriteLine("The current time is: " + DateTime.Now);
+            //Ask the user for a number
+            Console.WriteLine("\nEnter a number to see what the time will be in the many hours: ");
+            float answer = float.Parse(Console.ReadLine());
+            //Print what time it will be in the amount of hours the user input
+            Console.WriteLine("{0} + {1} hours(s) = {2}", DateTime.Now, answer, DateTime.Now.AddHours(answer));
+            Console.ReadLine();
+        }
+    }
+}
